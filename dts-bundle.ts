e166e39870a30d553d8f76f33335442b45ecb538
Lyove <lyove@outlook.com>
2023-05-24 18:04:03 +0800

    init

--- dts-bundle.ts
+++ dts-bundle.ts
@@ -1,11 +1,16 @@
 const outFileName = "editor.es";
 
 const config = {
+  compilationOptions: {
+    preferredConfigPath: "./tsconfig.json",
+  },
   entries: [
     {
       filePath: "./src/editor/index.tsx",
       outFile: `./lib/${outFileName}.d.ts`,
-      noCheck: false,
+      output: {
+        noBanner: true,
+      },
     },
   ],
 };
